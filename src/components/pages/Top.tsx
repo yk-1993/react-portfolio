da82ec0ebbfa80906dddd6b506abd7f9230a7834
DESKTOP-GTGJHIL\litbu <litbus19930529@yahoo.co.jp>
2021-10-22 13:13:00 +0900

    高さの調整

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,6 +1,12 @@
-import { Box } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
-
+import { ParallaxTest } from "../../motion/parallax/ParallaxTest";
 export const Top: VFC = memo(() => {
-  return <Box>Topページです</Box>;
+  return (
+    <>
+      <Flex height="70vh">
+        <ParallaxTest />
+      </Flex>
+    </>
+  );
 });
