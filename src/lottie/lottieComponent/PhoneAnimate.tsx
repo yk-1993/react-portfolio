7971c68724b0ed928ff9b03c1bd12e2996f318cf
DESKTOP-GTGJHIL\litbu <litbus19930529@yahoo.co.jp>
2021-11-08 17:41:13 +0900

    スマートフォンアニメーションを追加（React-lottie）

--- src/lottie/lottieComponent/PhoneAnimate.tsx
+++ src/lottie/lottieComponent/PhoneAnimate.tsx
@@ -1,9 +1,12 @@
 import animationData from "../phone.json";
 import Lottie from "react-lottie";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Button } from "@chakra-ui/button";
+import { useTransform, useViewportScroll } from "framer-motion";
 
 const defaultOptions = {
   loop: false,
-  autoplay: true,
+  autoplay: false,
   animationData: animationData,
   rendererSettings: {
     preserveAspectRatio: "xMidYMid slice",
@@ -11,5 +14,49 @@ const defaultOptions = {
 };
 
 export const PhoneAnimate = () => {
-  return <Lottie options={defaultOptions} height={500} width={500} />;
+  const [isDisplay, setIsDisplay] = useState(true);
+
+  const isRunning = useRef(false); // スクロール多発防止用フラグ
+
+  // リスナに登録する関数
+  const isScrollAnim = useCallback(() => {
+    if (isRunning.current) return;
+    isRunning.current = true;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    requestAnimationFrame(() => {
+      if (scrollTop > 2300) {
+        setIsDisplay(false);
+      } else {
+        setIsDisplay(true);
+      }
+      isRunning.current = false;
+    });
+  }, []);
+
+  // 登録と後始末
+  useEffect(() => {
+    document.addEventListener("scroll", isScrollAnim, { passive: false });
+    return () => {
+      document.removeEventListener("scroll", isScrollAnim, false);
+    };
+  }, []);
+  return (
+    <>
+      <Lottie
+        options={defaultOptions}
+        speed={2}
+        height="30vmin"
+        width="30vmin"
+        isStopped={isDisplay}
+        isClickToPauseDisabled={true}
+        ariaRole=""
+        eventListeners={[
+          {
+            eventName: "complete",
+            callback: () => setIsDisplay(false),
+          },
+        ]}
+      />
+    </>
+  );
 };
